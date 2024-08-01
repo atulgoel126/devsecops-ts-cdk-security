74c5617249dc1f01956b53cb61452804fdf85650
Atul Goel <atul.goel.126+github@gmail.com>
2024-08-01 10:46:01 +0530

    Reverting back to using account id for synth

--- src/apps/InfrastructureStackApp.ts
+++ src/apps/InfrastructureStackApp.ts
@@ -6,13 +6,13 @@ import { NetworkStack } from '../stacks/NetworkStack';
 const app = new cdk.App();
 
 const env = {
-    // account: process.env.CDK_DEFAULT_ACCOUNT || process.env.AWS_ACCOUNT_ID,
-    // region: process.env.CDK_DEFAULT_REGION || process.env.AWS_DEFAULT_REGION || 'us-east-1'
+    account: process.env.CDK_DEFAULT_ACCOUNT || process.env.AWS_ACCOUNT_ID,
+    region: process.env.CDK_DEFAULT_REGION || process.env.AWS_DEFAULT_REGION || 'us-east-1'
 };
 
-// if (!env.account || !env.region) {
-//     throw new Error('Please specify both CDK_DEFAULT_ACCOUNT and CDK_DEFAULT_REGION (or AWS_ACCOUNT_ID and AWS_DEFAULT_REGION)');
-// }
+if (!env.account || !env.region) {
+    throw new Error('Please specify both CDK_DEFAULT_ACCOUNT and CDK_DEFAULT_REGION (or AWS_ACCOUNT_ID and AWS_DEFAULT_REGION)');
+}
 
 const networkStack = new NetworkStack(app, 'NetworkStack', { env });
 new StorageStack(app, 'StorageStack', { env });
