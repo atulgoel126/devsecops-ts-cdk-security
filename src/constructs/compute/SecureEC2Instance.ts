a88cf76e315ccd286710fcf9281ca69cc1f1acc6
atulgoel126 <atul.goel.126@gmail.com>
2024-11-02 00:31:39 +0530

    Adding a custom IAM policy for the EC2 instance with minimum permissions

--- src/constructs/compute/SecureEC2Instance.ts
+++ src/constructs/compute/SecureEC2Instance.ts
@@ -1,16 +1,23 @@
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
+import {IRole} from "aws-cdk-lib/aws-iam";
+
+interface SecureEC2InstanceProps {
+    vpc: ec2.IVpc;
+    role?: IRole;
+}
 
 export class SecureEC2Instance extends Construct {
     public readonly instance: ec2.Instance;
 
-    constructor(scope: Construct, id: string, vpc: ec2.IVpc) {
+    constructor(scope: Construct, id: string, props: SecureEC2InstanceProps) {
         super(scope, id);
 
         this.instance = new ec2.Instance(this, 'SecureInstance', {
-            vpc,
+            vpc: props.vpc,
             instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
             machineImage: new ec2.AmazonLinuxImage(),
+            role: props.role, // Use the provided role or let CDK create a default one
         });
     }
 }
--- src/stacks/ComputeStack.ts
+++ src/stacks/ComputeStack.ts
@@ -2,14 +2,20 @@ import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 import {SecureEC2Instance} from "../constructs/compute/SecureEC2Instance";
+import {IRole} from "aws-cdk-lib/aws-iam";
 
 interface ComputeStackProps extends cdk.StackProps {
     vpc: ec2.IVpc;
+    ec2Role?: IRole;  // Add this line
 }
 
 export class ComputeStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props: ComputeStackProps) {
         super(scope, id, props);
-        new SecureEC2Instance(this, 'SecureEC2Instance', props.vpc);
+
+        new SecureEC2Instance(this, 'SecureEC2Instance', {
+            vpc: props.vpc,
+            role: props.ec2Role
+        });
     }
 }
